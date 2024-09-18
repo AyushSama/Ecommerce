2e628133852f846d208fb17113146d2641b232bd
AyushSama <aayush0029@gmail.com>
2024-09-18 18:01:31 +0530

    Structure Defined

---
+++ src/app/components/product-thumbnail/product-thumbnail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-thumbnail',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-thumbnail.component.html',
+  styleUrl: './product-thumbnail.component.scss'
+})
+export class ProductThumbnailComponent {
+
+}
